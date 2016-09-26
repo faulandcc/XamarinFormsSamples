6420dbcc0ee83cfc30b99e859dea16b1d1a26b46
Alexander Fauland <software@fauland.cc>
2016-09-26 14:31:45 +0200

    DEV GesturePattern

--- XamarinFormsSamples/XamarinFormsSamples/GesturePattern/GesturePatternSample.xaml.cs
+++ XamarinFormsSamples/XamarinFormsSamples/GesturePattern/GesturePatternSample.xaml.cs
@@ -4,10 +4,25 @@ namespace XamarinFormsSamples.GesturePattern
 {
 	public partial class GesturePatternSample : ContentPage
 	{
+		public GestureSampleViewModel ViewModel
+		{
+			get { return this.BindingContext as GestureSampleViewModel; }
+			set { this.BindingContext = value; }
+		}
+
+
 		public GesturePatternSample()
 		{
+			this.ViewModel = new GestureSampleViewModel();
+
 			InitializeComponent();
 			this.MyGesturePatternView.CreateTouchInterface();
+
+			// We can use the event to get the complete gesture or bind a view model (see label in XAML).
+			this.MyGesturePatternView.GesturePatternCompleted += (sender, args) =>
+			{
+				DisplayAlert("Gesture", args.GesturePatternValue, "Ok", "Cancel");
+			};
 		}
 	}
 }
---
+++ XamarinFormsSamples/XamarinFormsSamples/GesturePattern/GestureSampleViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using XamarinFormsSamples.Annotations;
+
+namespace XamarinFormsSamples.GesturePattern
+{
+	public class GestureSampleViewModel : INotifyPropertyChanged
+	{
+		private string _gestureValue;
+
+
+		#region properties
+
+		public string GestureValue
+		{
+			get { return _gestureValue; }
+			set
+			{
+				if (_gestureValue != value)
+				{
+					_gestureValue = value;
+					this.OnPropertyChanged();
+				}
+			}
+		}
+
+		#endregion
+
+
+		#region INotifyPropertyChanged
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		[NotifyPropertyChangedInvocator]
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		#endregion
+	}
+}
